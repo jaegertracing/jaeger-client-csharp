d429ebc904e99024bbf0951b07ee26895fcf0f6e
Jon Carl <jcarl@chathamfinancial.com>
2018-02-20 09:08:56 -0500

    add Field equality checking

--- src/LetsTrace/Field.cs
+++ src/LetsTrace/Field.cs
@@ -2,14 +2,16 @@ using System;
 
 namespace LetsTrace
 {
-    public abstract class Field
+    public abstract class Field : IEquatable<Field>
     {
         public string Key { get; set; }
         public TypeCode TypeCode { get; protected set; }
         public virtual string StringValue { get; }
+
+        public bool Equals(Field other) => other.Key == Key && other.TypeCode == TypeCode && other.StringValue == StringValue;
     }
 
-    public class Field<T> : Field
+    public class Field<T> : Field, IEquatable<Field<T>>
     {
         private T _value;
         public T Value { 
@@ -21,5 +23,7 @@ namespace LetsTrace
         }
 
         public override string StringValue { get { return _value.ToString(); } }
+
+        public bool Equals(Field<T> other) => Value is T && Value.Equals(other.Value) && base.Equals(other);
     }
 }
