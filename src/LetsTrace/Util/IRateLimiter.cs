bdb7d1d35b565799e7caba2bff77932dbefd0b75
Jon Carl <jcarl@chathamfinancial.com>
2018-02-20 11:19:47 -0500

    add a rate limiter util class

---
+++ src/LetsTrace/Util/IRateLimiter.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace LetsTrace.Util
+{
+    // IRateLimiter is a filter used to check if a message that is worth itemCost units is within the rate limits.
+    public interface IRateLimiter
+    {
+        bool CheckCredit(double itemCost);
+    }
+}
