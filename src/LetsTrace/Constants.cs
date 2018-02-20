393d851f84370b07042e17cb3a9b212596ce09f4
Jon Carl <jcarl@chathamfinancial.com>
2018-02-20 08:50:46 -0500

    add const sampler

--- src/LetsTrace/Constants.cs
+++ src/LetsTrace/Constants.cs
@@ -6,5 +6,14 @@ namespace LetsTrace
     {
         public const string TraceContextHeaderName = "X-LetsTrace-Trace-Context";
         public const string TraceBaggageHeaderPrefix = "X-LetsTrace-Baggage";
+
+        // SamplerTypeTagKey reports which sampler was used on the root span.
+        public const string SamplerTypeTagKey = "sampler.type";
+
+        // SamplerParamTagKey reports the parameter of the sampler, like sampling probability.
+        public const string SamplerParamTagKey = "sampler.param";
+
+        // SamplerTypeConst is the type of sampler that always makes the same decision.
+        public const string SamplerTypeConst = "const";
     }
 }
