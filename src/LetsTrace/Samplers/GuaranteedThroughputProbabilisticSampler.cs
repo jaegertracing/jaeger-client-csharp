6a38b12c6907a1cd0166c193e150507b3f5a65a4
Jon Carl <jcarl@chathamfinancial.com>
2018-02-28 14:59:36 -0500

    guaranteed throughput sampler bugfix
    we were not calling the Rate Limiting Sampler each time which would cause things not to work correctly

--- src/LetsTrace/Samplers/GuaranteedThroughputProbabilisticSampler.cs
+++ src/LetsTrace/Samplers/GuaranteedThroughputProbabilisticSampler.cs
@@ -6,9 +6,6 @@ namespace LetsTrace.Samplers
     // RateLimitingSampler. The RateLimitingSampler is used as a guaranteed lower bound sampler such that
     // every operation is sampled at least once in a time interval defined by the lowerBound. ie a lowerBound
     // of 1.0 / (60 * 10) will sample an operation at least once every 10 minutes.
-    //
-    // The ProbabilisticSampler is given higher priority when tags are emitted, ie. if IsSampled() for both
-    // samplers return true, the tags for ProbabilisticSampler will be used.
     public class GuaranteedThroughputProbabilisticSampler : ISampler
     {
         private ISampler _probabilisticSampler;
@@ -32,13 +29,14 @@ namespace LetsTrace.Samplers
 
         public (bool Sampled, IDictionary<string, Field> Tags) IsSampled(TraceId id, string operation)
         {
-            var isSampled = _probabilisticSampler.IsSampled(id, operation);
+            var probabilisticSampling = _probabilisticSampler.IsSampled(id, operation);
+            var rateLimitingSampling = _rateLimitingSampler.IsSampled(id, operation);
 
-            if (isSampled.Sampled) {
-                return isSampled;
+            if (probabilisticSampling.Sampled) {
+                return probabilisticSampling;
             }
 
-            return _rateLimitingSampler.IsSampled(id, operation);
+            return rateLimitingSampling;
         }
     }
 }
