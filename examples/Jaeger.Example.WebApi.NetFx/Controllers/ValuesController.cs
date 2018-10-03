135b8da7f5097e946dfd6604e47dd2df66c0e69f
Kraemer, Benjamin <benjamin.kraemer@jungheinrich.de>
2018-10-03 16:06:08 -0700

    Minor fixes

--- examples/Jaeger.Example.WebApi.NetFx/Controllers/ValuesController.cs
+++ examples/Jaeger.Example.WebApi.NetFx/Controllers/ValuesController.cs
@@ -9,7 +9,7 @@ namespace Jaeger.Example.WebApi.NetFx.Controllers
     [RoutePrefix("values")]
     public class ValuesController : ApiController
     {
-        private ITracer _tracer;
+        private readonly ITracer _tracer;
 
         public ValuesController(ITracer tracer)
         {
@@ -20,9 +20,9 @@ namespace Jaeger.Example.WebApi.NetFx.Controllers
         [Route("")]
         public IEnumerable<string> GetAll()
         {
-            using (var span1 = _tracer.BuildSpan("Controller-GetAll").StartActive(true))
+            using (var scope1 = _tracer.BuildSpan("Controller-GetAll").StartActive(true))
             {
-                using (var span2 = _tracer.BuildSpan("DataSource-GetAll").StartActive(true))
+                using (var scope2 = _tracer.BuildSpan("DataSource-GetAll").StartActive(true))
                 {
                     Thread.Sleep(TimeSpan.FromSeconds(1));
 
