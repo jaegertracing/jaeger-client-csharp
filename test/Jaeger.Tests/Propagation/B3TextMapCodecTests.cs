0794ea71cb6e58f7bf0f0ef2c0c8ceceb1d8b6d9
Benjamin Krämer <benjamin.kraemer@alien-scripts.de>
2019-04-23 05:50:43 +0200

    Added support for 128bit to B3 decoder (#144)

--- test/Jaeger.Tests/Propagation/B3TextMapCodecTests.cs
+++ test/Jaeger.Tests/Propagation/B3TextMapCodecTests.cs
@@ -1,30 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using Jaeger.Propagation;
 using OpenTracing.Propagation;
 using Xunit;
 
 namespace Jaeger.Tests.Propagation
 {
-    /**
-     * NOTE:
-     * These tests are based on the ones from jaeger-b3, and included to improve the test
-     * coverage. The main testing of the B3TextMapCodec is still performed via the tests
-     * in the jaeger-b3 module.
-     *
-     */
     public class B3TextMapCodecTest
     {
         B3TextMapCodec b3Codec = new B3TextMapCodec();
 
         [Fact]
-        public void Downgrades128BitTraceIdToLower64Bits()
+        public void Supports64BitTraceId()
         {
-            string hex128Bits = "463ac35c9f6413ad48485a3953bb6124";
+            string upper64Bits = "463ac35c9f6413ad";
             string lower64Bits = "48485a3953bb6124";
 
             DelegatingTextMap textMap = new DelegatingTextMap();
-            textMap.Set(B3TextMapCodec.TraceIdName, hex128Bits);
+            textMap.Set(B3TextMapCodec.TraceIdName, upper64Bits);
             textMap.Set(B3TextMapCodec.SpanIdName, lower64Bits);
             textMap.Set(B3TextMapCodec.ParentSpanIdName, "0");
             textMap.Set(B3TextMapCodec.SampledName, "1");
@@ -32,9 +26,32 @@ namespace Jaeger.Tests.Propagation
 
             SpanContext context = b3Codec.Extract(textMap);
 
-            //Assert.NotNull(HexCodec.LowerHexToUnsignedLong(lower64Bits));
-            Assert.Equal(HexCodec.LowerHexToUnsignedLong(lower64Bits), context.TraceId.Low);
-            Assert.Equal(HexCodec.LowerHexToUnsignedLong(lower64Bits), context.SpanId);
+            Assert.Equal(0L, context.TraceId.High);
+            Assert.Equal(long.Parse(upper64Bits, NumberStyles.HexNumber), context.TraceId.Low);
+            Assert.Equal(long.Parse(lower64Bits, NumberStyles.HexNumber), context.SpanId);
+            Assert.Equal(new SpanId(0), context.ParentId);
+            Assert.True(context.Flags.HasFlag(SpanContextFlags.Sampled));
+            Assert.True(context.Flags.HasFlag(SpanContextFlags.Debug));
+        }
+
+        [Fact]
+        public void Supports128BitTraceId()
+        {
+            string hex128Bits = "463ac35c9f6413ad48485a3953bb6124";
+            string upper64Bits = "463ac35c9f6413ad";
+            string lower64Bits = "48485a3953bb6124";
+
+            DelegatingTextMap textMap = new DelegatingTextMap();
+            textMap.Set(B3TextMapCodec.TraceIdName, hex128Bits);
+            textMap.Set(B3TextMapCodec.SpanIdName, lower64Bits);
+            textMap.Set(B3TextMapCodec.SampledName, "1");
+            textMap.Set(B3TextMapCodec.FlagsName, "1");
+
+            SpanContext context = b3Codec.Extract(textMap);
+
+            Assert.Equal(long.Parse(upper64Bits, NumberStyles.HexNumber), context.TraceId.High);
+            Assert.Equal(long.Parse(lower64Bits, NumberStyles.HexNumber), context.TraceId.Low);
+            Assert.Equal(long.Parse(lower64Bits, NumberStyles.HexNumber), context.SpanId);
             Assert.Equal(new SpanId(0), context.ParentId);
             Assert.True(context.Flags.HasFlag(SpanContextFlags.Sampled));
             Assert.True(context.Flags.HasFlag(SpanContextFlags.Debug));
